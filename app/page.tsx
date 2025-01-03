7e218fc7ee1738d5ed4b8c6831f8be9f8dec470d
calculus0129 <ryanbhang129@naver.com>
2025-01-03 11:12:36 +0900

    refactor: migrate footer page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { CORE_CONCEPTS, EXAMPLES } from "@/app/data";
 import TabButton from "@/components/TabButton";
 import TabContent from "@/components/TabContent";
 import { useEffect, useState } from "react";
+import Footer from "@/components/Footer";
+// import "./page.css";
 
 const reactDescriptions = ["Advanced", "Basic", "Core", "Fundamental"];
 
@@ -110,50 +112,7 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer className={styles.footer}/>
     </div>
   );
 }
