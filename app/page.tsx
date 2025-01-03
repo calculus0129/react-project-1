6357af2b7778d06fbb81575cd2ae7f22765980b6
calculus0129 <ryanbhang129@naver.com>
2025-01-03 11:30:49 +0900

    style: fix @typescript-eslint/no-unused-vars

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import CoreConcept from "@/components/CoreConcept";
